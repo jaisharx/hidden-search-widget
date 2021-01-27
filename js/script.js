2b19d641fe9f617602bca5e47bb215c9f5a0a318
jai-sharma <sendtojsharma@gmail.com>
2021-01-27 10:23:08 +0530

    added hidden search bar with custom styles

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-// type your js logic here...
+const search = document.querySelector('.search');
+const input = document.querySelector('.input');
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+    search.classList.toggle('active');
+    input.focus();
+});
